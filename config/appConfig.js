21942edd06f6bec2f26898771e3eeb483abd9874
Kevin Hering <kevin@kevinhering.com>
2018-07-31 22:38:07 -0400

    env changes for Heroku

--- config/appConfig.js
+++ config/appConfig.js
@@ -1,16 +1,18 @@
 // env variables
 const dotenv = require('dotenv');
-const { parsed } = dotenv.config();
+const { parsed } = dotenv.config({ silent: process.env.NODE_ENV === 'production' });
+
+const isProd = process.env.NODE_ENV === 'production';
 
 const config = {
   getKey: function() {
-    return parsed.KEY;
+    return isProd ? process.env.KEY : parsed.KEY;
   },
 
   getMongoURI: function() {
-    const user = parsed.DB_USER;
-    const pwd = parsed.DB_PASS;
-    const host = parsed.DB_HOST;
+    const user = isProd ? process.env.DB_USER : parsed.DB_USER;
+    const pwd = isProd ? process.env.DB_PASS : parsed.DB_PASS;
+    const host = isProd ? process.env.DB_HOST : parsed.DB_HOST;
     return `mongodb://${user}:${pwd}@${host}`;
   },
 
